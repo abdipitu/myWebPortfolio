0a92a5932503c15853957c69ad93cc16af671c4f
abdipitu <demitkipuw@gmail.com>
2024-01-24 21:22:04 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ['Inter'],
+        'poppins': ['Poppins'],
+        'mulish': ['Mulish'],
+      }
+    },
+  },
+  plugins: [],
+}
+
